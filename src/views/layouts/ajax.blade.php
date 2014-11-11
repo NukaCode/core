ac2bd53247f6224a6a768491e6656914fd46e546
stygiansabyss <stygian.warlock.v2@gmail.com>
2014-11-11 16:14:56 -0600

    Adding boot box classes to ajax layout

--- src/views/layouts/ajax.blade.php
+++ src/views/layouts/ajax.blade.php
@@ -17,6 +17,48 @@
 
 <script>
     $(document).ready(function() {
+        bootbox.setDefaults({backdrop: false});
+
+        $("a.confirm-remove").click(function(e) {
+            e.preventDefault();
+            var location = $(this).attr('href');
+            bootbox.dialog({
+                message: "Are you sure you want to remove this item?",
+                buttons: {
+                    success: {
+                        label: "Yes",
+                        className: "btn-primary",
+                        callback: function() {
+                            window.location.replace(location);
+                        }
+                    },
+                    danger: {
+                        label: "No",
+                        className: "btn-primary"
+                    }
+                }
+            });
+        });
+        $("a.confirm-continue").click(function(e) {
+            e.preventDefault();
+            var location = $(this).attr('href');
+            bootbox.dialog({
+                message: "Are you sure you want to continue?",
+                buttons: {
+                    danger: {
+                        label: "No",
+                        className: "btn-primary"
+                    },
+                    success: {
+                        label: "Yes",
+                        className: "btn-primary",
+                        callback: function() {
+                            window.location.replace(location);
+                        }
+                    },
+                }
+            });
+        });
         // On Ready Js
         @section('onReadyJs')
         @show
