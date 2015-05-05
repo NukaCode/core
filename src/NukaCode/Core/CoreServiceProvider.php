f4a66d019aa18d661b2bc54025a67eea476cebb0
Stygian <stygian.warlock.v2@gmail.com>
2015-05-05 13:18:26 -0500

    Update CoreServiceProvider.php

--- src/NukaCode/Core/CoreServiceProvider.php
+++ src/NukaCode/Core/CoreServiceProvider.php
@@ -11,7 +11,7 @@ class CoreServiceProvider extends BaseServiceProvider {
 
     const NAME = 'core';
 
-    const VERSION = '2.0.10';
+    const VERSION = '2.0.11';
 
     const DOCS = 'nukacode-core';
 
