7cdec48278c6405c40ad6a3c688d85a53d0cbccc
Travis Blasingame <stygian.warlock.v2@gmail.com>
2016-12-05 08:13:08 -0600

    Create AutoViewCollector.php

---
+++ src/NukaCode/Core/Providers/AutoViewCollector.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace NukaCode\Core\Providers;
+
+use DebugBar\Bridge\Twig\TwigCollector;
+use DebugBar\DataCollector\Renderable;
+use NukaCode\Core\View\Models\ViewModel;
+
+class AutoViewCollector extends TwigCollector implements Renderable
+{
+    /**
+     * @var null|\NukaCode\Core\View\Models\ViewModel
+     */
+    public $viewModel = null;
+
+    /**
+     * Create a ViewCollector
+     */
+    public function __construct()
+    {
+        $this->name = 'auto_views';
+    }
+
+    public function getName()
+    {
+        return 'auto_views';
+    }
+
+    public function getWidgets()
+    {
+        return [
+            'auto_resolved_view' => [
+                'icon'    => 'archive',
+                'widget'  => 'PhpDebugBar.Widgets.VariableListWidget',
+                'map'     => 'auto_views',
+                'default' => '{}',
+            ],
+        ];
+    }
+
+    public function addDetails(ViewModel $viewModel)
+    {
+        $this->viewModel = $viewModel;
+    }
+
+    public function collect()
+    {
+        $data = [
+            'resolved view'     => $this->viewModel->view,
+            'resolution type'   => $this->viewModel->type,
+            'attempted views'   => $this->getDataFormatter()
+                                        ->formatVar($this->viewModel->attemptedViews->toArray()),
+            'controller'        => $this->viewModel->controller,
+            'action'            => is_null($this->viewModel->action) ? 'null (__invoke)' : $this->viewModel->action,
+            'final prefix'      => $this->viewModel->prefix,
+            'possible prefixes' => $this->getDataFormatter()
+                                        ->formatVar($this->viewModel->prefixes->toArray()),
+            'config index'      => $this->viewModel->configIndex,
+        ];
+
+        return $data;
+    }
+}
